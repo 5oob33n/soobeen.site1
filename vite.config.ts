6141830b5ea50ebc7afda9c4773c44e4ad1ce8bb
Soobeen Woo <swoo@uk-video-mbp.local>
2025-12-06 00:50:08 +0100

    Fix GitHub Pages asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages에서 프로젝트가 올라가는 기본 경로
+  // 주소: https://5oob33n.github.io/soobeen-woo-portfolio
   base: '/soobeen-woo-portfolio/',
   build: {
-    outDir: 'docs'  // build output folder will be "docs"
+    // GitHub Pages용으로 main 브랜치의 docs 폴더에 빌드
+    outDir: 'docs'
   }
 })
